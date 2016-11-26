49f50b3a2ef6a1f91e4e06282adb916a86c20470
Tommaso <vannozzi.tommaso@gmail.com>
2016-11-26 13:57:37 +0100

    Aggiunta Form

---
+++ Progetto5B/Progetto5B/FormVannozzi.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Progetto5B
+{
+    partial class FormVannozzi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormVannozzi));
+            this.btnChiudi = new System.Windows.Forms.Button();
+            this.txtDataNascita = new System.Windows.Forms.TextBox();
+            this.txtLuogoNascita = new System.Windows.Forms.TextBox();
+            this.txtCognome = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnChiudi
+            // 
+            this.btnChiudi.Location = new System.Drawing.Point(342, 256);
+            this.btnChiudi.Name = "btnChiudi";
+            this.btnChiudi.Size = new System.Drawing.Size(75, 23);
+            this.btnChiudi.TabIndex = 20;
+            this.btnChiudi.Text = "Chiudi";
+            this.btnChiudi.UseVisualStyleBackColor = true;
+            this.btnChiudi.Click += new System.EventHandler(this.btnChiudi_Click);
+            // 
+            // txtDataNascita
+            // 
+            this.txtDataNascita.Location = new System.Drawing.Point(317, 136);
+            this.txtDataNascita.Name = "txtDataNascita";
+            this.txtDataNascita.ReadOnly = true;
+            this.txtDataNascita.Size = new System.Drawing.Size(100, 20);
+            this.txtDataNascita.TabIndex = 19;
+            this.txtDataNascita.Text = "21/07/1998";
+            // 
+            // txtLuogoNascita
+            // 
+            this.txtLuogoNascita.Location = new System.Drawing.Point(317, 181);
+            this.txtLuogoNascita.Name = "txtLuogoNascita";
+            this.txtLuogoNascita.ReadOnly = true;
+            this.txtLuogoNascita.Size = new System.Drawing.Size(100, 20);
+            this.txtLuogoNascita.TabIndex = 18;
+            this.txtLuogoNascita.Text = "Firenze";
+            // 
+            // txtCognome
+            // 
+            this.txtCognome.Location = new System.Drawing.Point(317, 87);
+            this.txtCognome.Name = "txtCognome";
+            this.txtCognome.ReadOnly = true;
+            this.txtCognome.Size = new System.Drawing.Size(100, 20);
+            this.txtCognome.TabIndex = 17;
+            this.txtCognome.Text = "Vannozzi";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(317, 46);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.ReadOnly = true;
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 16;
+            this.txtNome.Text = "Tommaso";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(192, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 15);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Luogo di nascita:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(192, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Cognome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(192, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Data di nascita";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(192, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Nome:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(17, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(155, 155);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormVannozzi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 324);
+            this.Controls.Add(this.btnChiudi);
+            this.Controls.Add(this.txtDataNascita);
+            this.Controls.Add(this.txtLuogoNascita);
+            this.Controls.Add(this.txtCognome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormVannozzi";
+            this.Text = "FormVannozzi";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChiudi;
+        private System.Windows.Forms.TextBox txtDataNascita;
+        private System.Windows.Forms.TextBox txtLuogoNascita;
+        private System.Windows.Forms.TextBox txtCognome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Progetto5B/Progetto5B/FormVannozzi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Progetto5B
+{
+    public partial class FormVannozzi : Form
+    {
+        public FormVannozzi()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChiudi_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
