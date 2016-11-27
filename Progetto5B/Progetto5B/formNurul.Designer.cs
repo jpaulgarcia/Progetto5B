4f524f360e65b8fe911ae6a370bd622f40e480b0
Nurul-Repo <najmul.nurul@gmail.com>
2016-11-27 15:06:55 +0100

    Aggiunta foto

--- Progetto5B/Progetto5B/formNurul.Designer.cs
+++ Progetto5B/Progetto5B/formNurul.Designer.cs
@@ -28,11 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(155, 155);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // formNurul
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "formNurul";
+            this.ClientSize = new System.Drawing.Size(424, 258);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "formNurul";
+            this.Text = "Nurul";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
