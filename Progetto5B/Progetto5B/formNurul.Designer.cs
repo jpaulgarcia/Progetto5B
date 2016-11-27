d92c16cfafa9ca5455db916792436d5d5534f4e3
Nurul-Repo <najmul.nurul@gmail.com>
2016-11-27 15:15:47 +0100

    Aggiunta luogo di nascita

--- Progetto5B/Progetto5B/formNurul.Designer.cs
+++ Progetto5B/Progetto5B/formNurul.Designer.cs
@@ -35,13 +35,15 @@
             this.txtCognome = new System.Windows.Forms.TextBox();
             this.lblData = new System.Windows.Forms.Label();
             this.txtData = new System.Windows.Forms.TextBox();
+            this.lblLuogo = new System.Windows.Forms.Label();
+            this.txtLuogo = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Progetto5B.Properties.Resources.Nurul;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 29);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(130, 151);
             this.pictureBox1.TabIndex = 0;
@@ -104,11 +106,32 @@
             this.txtData.TabIndex = 10;
             this.txtData.Text = "22/10/1996";
             // 
+            // lblLuogo
+            // 
+            this.lblLuogo.AutoSize = true;
+            this.lblLuogo.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLuogo.Location = new System.Drawing.Point(181, 165);
+            this.lblLuogo.Name = "lblLuogo";
+            this.lblLuogo.Size = new System.Drawing.Size(97, 15);
+            this.lblLuogo.TabIndex = 11;
+            this.lblLuogo.Text = "Luogo di nascita:";
+            // 
+            // txtLuogo
+            // 
+            this.txtLuogo.Location = new System.Drawing.Point(295, 165);
+            this.txtLuogo.Name = "txtLuogo";
+            this.txtLuogo.ReadOnly = true;
+            this.txtLuogo.Size = new System.Drawing.Size(100, 20);
+            this.txtLuogo.TabIndex = 12;
+            this.txtLuogo.Text = "Bangladesh";
+            // 
             // formNurul
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(424, 258);
+            this.Controls.Add(this.txtLuogo);
+            this.Controls.Add(this.lblLuogo);
             this.Controls.Add(this.txtData);
             this.Controls.Add(this.lblData);
             this.Controls.Add(this.txtCognome);
@@ -133,5 +156,7 @@
         private System.Windows.Forms.TextBox txtCognome;
         private System.Windows.Forms.Label lblData;
         private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Label lblLuogo;
+        private System.Windows.Forms.TextBox txtLuogo;
     }
 }
